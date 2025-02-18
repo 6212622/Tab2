a81017e1d89b9e81dea77eb4399c8d1adf89749d
!Shahzod Davlatov <!sh.davlatov@tmz.local>
2025-02-18 08:12:04 +0500

    initial

--- AttendanceApp/employee_data/models.py
+++ AttendanceApp/employee_data/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-class EmployeeData(models.Model):
-    tabnumber = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
-    # Добавьте другие поля здесь
+class ShiftSchedule(models.Model):
+    date = models.DateField()
+    shift_1_brigade_1 = models.CharField(max_length=255, blank=True, null=True)
+    shift_1_brigade_2 = models.CharField(max_length=255, blank=True, null=True)
+    shift_2_brigade_1 = models.CharField(max_length=255, blank=True, null=True)
+    shift_2_brigade_2 = models.CharField(max_length=255, blank=True, null=True)
+    shift_2_brigade_3 = models.CharField(max_length=255, blank=True, null=True)
+    shift_2_brigade_4 = models.CharField(max_length=255, blank=True, null=True)
+    shift_3 = models.CharField(max_length=255, blank=True, null=True)
+    shift_5_brigade_1 = models.CharField(max_length=255, blank=True, null=True)
+    shift_5_brigade_2 = models.CharField(max_length=255, blank=True, null=True)
+    shift_6 = models.CharField(max_length=255, blank=True, null=True)
+    shift_7 = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.date} - {self.shift_1_brigade_1} - {self.shift_1_brigade_2} - {self.shift_2_brigade_1} - {self.shift_2_brigade_2} - {self.shift_2_brigade_3} - {self.shift_2_brigade_4} - {self.shift_3} - {self.shift_5_brigade_1} - {self.shift_5_brigade_2} - {self.shift_6} - {self.shift_7}"
